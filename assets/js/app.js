7696c0a0e5ca73dac796ebcf58e51e7a65c41299
Richard Tafurth-Garcia <git@tgarcia.ch>
2019-12-14 12:14:36 +0100

    Correction du timestamp et du for (c'est of et pas in)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
+"use strict"
+
 import Typed from 'typed.js';
 
-var typed = new Typed('#dynamic', {
+let typed = new Typed('#dynamic', {
     strings: ['Libéral.', 'Anti-consumériste.', 'Pro-décentralisation.', 'Marathonien (ou presque)'],
     smartBackspace: true, // Default value
     typeSpeed: 80,
@@ -9,16 +11,17 @@ var typed = new Typed('#dynamic', {
 
 async function getContributions() {
     function countContributions(since, contributions) {
-        var count = 0
-        for (var contribution in contributions) {
-            if (new Date(contribution) - since > 0)
-                count += contributions[contribution]
+        let count = 0
+        for (let contribution of contributions) {
+            // puisque le timestamp ne se base pas sur le timestamp unix, je l'adaptes
+            if (new Date(contribution.timestamp * 1000) > since)
+                count += contribution.contributions
         }
 
         return count
     }
 
-    const response = await fetch('https://git.spätz.li/api/v1/users/richard/heatmap', {
+    let response = await fetch('https://git.spätz.li/api/v1/users/richard/heatmap', {
         method: 'GET',
         mode: 'cors',
         headers:{
@@ -32,15 +35,15 @@ async function getContributions() {
 
     const contributions = await response.json()
 
-    var yearlyParagraph = document.getElementById('ycontrib').children[0]
-    var monthlyParagraph = document.getElementById('mcontrib').childNodes[0]
-    var weeklyParagraph = document.getElementById('wcontrib').childNodes[0]
+    let yearlyParagraph = document.getElementById('ycontrib').children[0]
+    let monthlyParagraph = document.getElementById('mcontrib').childNodes[0]
+    let weeklyParagraph = document.getElementById('wcontrib').childNodes[0]
 
-    var lastYear = new Date()
+    let lastYear = new Date()
     lastYear.setFullYear( lastYear.getFullYear() - 1 )
-    var lastMonth = new Date()
+    let lastMonth = new Date()
     lastMonth.setMonth( lastMonth.getMonth() - 1 )
-    var lastWeek = new Date()
+    let lastWeek = new Date()
     lastWeek.setDate( lastWeek.getDate() - 7 )
 
     yearlyParagraph.textContent = countContributions(lastYear, contributions)
