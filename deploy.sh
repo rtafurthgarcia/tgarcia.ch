0631c4972710f017b436759875591cb0f9dde9a9
Richard Tafurth-Garcia <git@tgarcia.ch>
2020-04-23 23:31:28 +0200

    Oopsie au niveau des répertoires

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,8 @@ lxc file push -r public/* mothergoose:yunohost/var/www/webapp_adidal/tgarcia.ch_
 lxc exec mothergoose:yunohost -- systemctl reload nginx
 lxc exec mothergoose:yunohost -- chmod 644 -R /var/www/webapp_adidal/tgarcia.ch_
 lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_
+lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_/css
+lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_/fonts
+lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_/images
+lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_/js
 lxc exec mothergoose:yunohost -- chown -R www-data:www-data /var/www/webapp_adidal/tgarcia.ch_
