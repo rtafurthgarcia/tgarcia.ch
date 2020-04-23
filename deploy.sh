0ba9a33df5e05c72ef099b0012d517b48deb70c0
Richard Tafurth-Garcia <git@tgarcia.ch>
2020-04-23 23:20:12 +0200

    Restrictions supplémentaires au niveau des permissions du dossier nginx

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
 lxc exec mothergoose:yunohost -- rm -rf /var/www/webapp_adidal/tgarcia.ch_/*
 lxc file push -r public/* mothergoose:yunohost/var/www/webapp_adidal/tgarcia.ch_/.
 lxc exec mothergoose:yunohost -- systemctl reload nginx
+lxc exec mothergoose:yunohost -- chmod 644 -R /var/www/webapp_adidal/tgarcia.ch_
+lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_
+lxc exec mothergoose:yunohost -- chown -R www-data:www-data /var/www/webapp_adidal/tgarcia.ch_
