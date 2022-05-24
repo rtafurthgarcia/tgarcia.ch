12f5353d65114d34bd1537109416a3649e7d5f2b
Richard Tafurth-Garcia <git@tgarcia.ch>
2022-05-24 19:22:47 +0200

    Màj des paquets. Adaptation pour le NAS synology au niveau du déploiement. Nouveau CV. Changement de l'arborescence par défaut pour les fonts.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-lxc exec mothergoose:yunohost -- rm -rf /var/www/webapp_adidal/tgarcia.ch_/*
-lxc file push -r public/* mothergoose:yunohost/var/www/webapp_adidal/tgarcia.ch_/.
-lxc exec mothergoose:yunohost -- systemctl restart nginx
-lxc exec mothergoose:yunohost -- chmod 644 -R /var/www/webapp_adidal/tgarcia.ch_
-lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_
-lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_/css
-lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_/fonts
-lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_/images
-lxc exec mothergoose:yunohost -- chmod 755 /var/www/webapp_adidal/tgarcia.ch_/js
-lxc exec mothergoose:yunohost -- chown -R www-data:www-data /var/www/webapp_adidal/tgarcia.ch_
-lxc exec mothergoose:yunohost -- systemctl restart nginx
+ssh nas "rm -rf /volume1/web/*"
+scp -P 4269 -r public/* nas:/volume1/web/.
+ssh nas "cd /volume1/web && find . -type d -exec chmod 0750 {} \\;"
+ssh nas "cd /volume1/web && find . -type f -exec chmod 0640 {} \\;"
