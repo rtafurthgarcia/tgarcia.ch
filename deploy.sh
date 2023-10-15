bedf7e39c1576e64d9442440057d24e1ed42f196
Richard Tafurth-Garcia <git@tgarcia.ch>
2023-10-15 17:33:12 +0000

    Suppression de deploy.sh (est désormais fait manuellement)

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ssh nas "rm -rf /volume1/web/*"
-scp -P 4269 -r public/* nas:/volume1/web/.
-ssh nas "cd /volume1/web && find . -type d -exec chmod 0750 {} \\;"
-ssh nas "cd /volume1/web && find . -type f -exec chmod 0640 {} \\;"
